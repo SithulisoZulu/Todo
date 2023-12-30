45aa608a2917fb98a8697ddc0783831e412b09be
Sithuliso Mondli Zulu <Sithulisomz@dutappfactory.tech>
2023-12-31 01:12:24 +0200

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
-
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
 import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), react()]
-  
+  integrations: [tailwind(), react(), sentry(), spotlightjs()],
+  integrations: [sentry(), spotlightjs()]
 });
